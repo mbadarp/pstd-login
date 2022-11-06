2ae43c19bffedf9c432108200892de81ef06691c
mbadarp <mbadarp@gmail.com>
2022-11-06 16:04:39 +0700

    menu management part 1

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -15,27 +15,49 @@
 
     <div class="menu-inner-shadow"></div>
 
-    <ul class="menu-inner py-1">
-        <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">Administrator</span>
-        </li>
-        <!-- Dashboard -->
-        <li class="menu-item">
-            <a href="index.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
-                <div data-i18n="Analytics">Dashboard</div>
-            </a>
-        </li>
+    <!-- QUERY MENU -->
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `tb_user_menu`.`id`, `menu`
+                        FROM `tb_user_menu` JOIN `tb_user_access_menu` 
+                        ON `tb_user_menu`.`id` = `tb_user_access_menu`.`menu_id`
+                        WHERE `tb_user_access_menu`.`role_id` = $role_id
+                        ORDER BY `tb_user_access_menu`.`menu_id` ASC";
+    $menu = $this->db->query($queryMenu)->result_array();
+    ?>
 
-        <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">User</span>
-        </li>
-        <li class="menu-item">
-            <a href="index.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bxs-user"></i>
-                <div data-i18n="Analytics">My profile</div>
-            </a>
-        </li>
+    <ul class="menu-inner py-1">
+        <!-- LOOPING MENU -->
+        <?php foreach ($menu as $m) : ?>
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text"><?= $m['menu']; ?></span>
+            </li>
+
+            <!-- SUB MENU according to MENU  -->
+            <?php
+            $menuId = $m['id'];
+            $querySubMenu = "SELECT * FROM `tb_user_sub_menu`
+                            WHERE `menu_id` = $menuId
+                            AND `is_active` = 1";
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+            ?>
+
+            <?php foreach ($subMenu as $sm) : ?>
+                <li class="menu-item">
+                    <a href="<?= base_url($sm['url']); ?>" class="menu-link">
+                        <i class="menu-icon tf-icons <?= $sm['icon']; ?>"></i>
+                        <div data-i18n="Analytics"><?= $sm['title']; ?></div>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+
+
+        <?php endforeach; ?>
+
+
+
+
+
 
         <!-- Misc -->
         <li class="menu-header small text-uppercase"><span class="menu-header-text"></span></li>
