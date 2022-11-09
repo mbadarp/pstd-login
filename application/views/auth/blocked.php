b3fd8149e24856e092ca9b4803d5a40cb72fba80
mbadarp <mbadarp@gmail.com>
2022-11-09 13:12:54 +0700

    8 - user access

---
+++ application/views/auth/blocked.php
@@ -0,0 +1,25 @@
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <!-- Content -->
+
+    <div class="container-fluid flex-grow-1 container-p-y">
+        <div class="col-md-12">
+            <div class="col-middle">
+                <div class="text-center text-center mt-5">
+                    <h1 class="display-1">403</h1>
+                    <h2><?= $title; ?></h2>
+                    <p>It is not your right to access this page!.
+                    </p>
+
+
+                    <a href="<?= base_url('user'); ?>">
+                        <button type="button" class="btn btn-primary me-2">
+                            <span class="tf-icons bx bx-left-arrow-alt"></span> Back to Dashboard
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- / Content -->
--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -32,7 +32,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="<?= base_url('assets/'); ?>/assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="<?= base_url('assets/'); ?>assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -43,25 +43,25 @@
     />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href=".<?= base_url('assets/'); ?>/assets/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href=".<?= base_url('assets/'); ?>assets/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>/assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>/assets/css/demo.css" />
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
     <!-- Page CSS -->
     <!-- Page -->
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>/assets/vendor/css/pages/page-auth.css" />
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>assets/vendor/css/pages/page-auth.css" />
     <!-- Helpers -->
-    <script src="<?= base_url('assets/'); ?>/assets/vendor/js/helpers.js"></script>
+    <script src="<?= base_url('assets/'); ?>assets/vendor/js/helpers.js"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url('assets/'); ?>/assets/js/config.js"></script>
+    <script src="<?= base_url('assets/'); ?>assets/js/config.js"></script>
   </head>
 
   <body>
