5a36e2cff041f8a672c69fd165c93856dbc40a01
mbadarp <mbadarp@gmail.com>
2022-11-09 17:12:55 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,7 +11,7 @@ class Auth extends CI_Controller
 
     public function index()
     {
-        $this->goToDefaultPage();
+        $this->_verifyAccess();
 
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
         $this->form_validation->set_rules('password', 'Password', 'required|trim');
@@ -67,7 +67,7 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        $this->goToDefaultPage();
+        $this->_verifyAccess();
 
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[tb_user.email]', [
@@ -118,7 +118,7 @@ class Auth extends CI_Controller
         $this->load->view('templates/auth_footer');
     }
 
-    private function goToDefaultPage()
+    private function _verifyAccess()
     {
         $level = $this->session->userdata('role_id');
 
